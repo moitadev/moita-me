aee26b51d3ada3a0cdff85fd581a7e07059b892f
Marcos Sales <katakenshi@icloud.com>
2021-09-15 06:36:55 -0300

    Added button and smooth scroll to work

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,20 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <div className="container m-auto pt-8 p-4 max-w-9xl flex flex-col">
-      <header className="flex justify-between items-center">
-        <div className="flex">
-          <img src="/assets/icon.svg" width={50}/>
-          <img className="px-2 inline-block hidden sm:inline-block" src="/assets/textNegative.svg" width={100} />
+      <header className="flex flex-row justify-between items-center bg-moitablack">
+        <div className="flex flex-row items-center">
+          <Link href="/">
+            <a><img src="/assets/icon.svg" width={50}/></a>
+          </Link>
+          <Link href="/">
+            <a><img className="p-2 hidden sm:inline-block" src="/assets/textNegative.svg" width={100} /></a>
+          </Link>
         </div>
         <nav className="flex my-4">
           <ul className="space-x-4 col-end-7 inline-flex items-center">
             <li>
               <Link href="/about">
-                <a>sobre</a>
+                <a>about</a>
               </Link>
             </li>
           </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head';
+import React, { useRef } from "react";
 import Link from 'next/Link';
 import { worksData } from '../lib/data';
 
 export default function Home() {
+  const workSection = useRef(null);
+
+  const gotoWorkSection = () => window.scrollTo({ 
+    top: workSection.current.offsetTop, 
+    behavior: "smooth"
+  });
+
   return (
     <div>
       <Head>
@@ -10,14 +18,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container p-2 flex flex-row justify-center h-screen items-center">
-        <div className="items-center">
-        Olá, eu sou o <b>Moita</b>!<hr className="border-2 border-solid m-1 w-3/6 border-moitagreen mx-4 items-center hidden sm:inline-flex" /><br />
-        sou um <b>desenvolvedor web</b> e estou aqui para <b>ajudar você</b>.
+      <main className="container p-2 flex flex-col justify-center h-screen items-center">
+        <div className="flex-col items-center antialiased">
+          Hey, I'm <b>Moita</b>!<hr className="border-2 border-solid m-1 w-3/6 border-moitagreen mx-4 items-center hidden sm:inline-flex" /><br />
+          I'm a <b>web developer</b> and I'm here to <b>help you</b>.
+        </div>
+        <div className="items-center flex-col my-8">
+          <button class="bg-transparent text-white transition duration-500 ease-in-out hover:text-moitagreen py-2 px-4 border-4 border-solid border-white hover:border-moitagreen" onClick={gotoWorkSection}>
+            my work
+          </button>
         </div>
       </main>
 
-      <div className="justify-center h-screen">
+      <div ref={workSection} className="container m-auto pt-8 p-4 justify-center max-w-9xl flex flex-col h-screen">
         {worksData.map((item) => (
           <WorkListItem key={item.slug} {...item} />
         ))}
@@ -28,10 +41,8 @@ export default function Home() {
 
 function WorkListItem({ slug, title }) {
   return(
-      <div>
-        <Link href={`/works/${slug}`}>
-          <a className="text-9xl text-left">{title}</a>
-        </Link>
-      </div>
+    <Link href={`/works/${slug}`}>
+      <a className="sm:text-9xl text-4xl p-3 subpixel-antialiased text-left">{title}</a>
+    </Link>
   );
 }
